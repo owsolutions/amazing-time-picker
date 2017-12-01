1fef836475340b163ee3821a7ccdb467fbb07685
anonymouse <anonymouse>
2017-12-02 06:21:05 +0800

    Add index.ts to export the module

---
+++ index.ts
@@ -0,0 +1,2 @@
+export * from './src/app/app.module';
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,4 +27,4 @@ import { MatTimepickerDirective } from './mat-timepicker.directive';
     TimePickerComponent
   ]
 })
-export class AppModule { }
+export class MatTimePicker { }
