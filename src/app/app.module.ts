4257c6ada1e1ba4e2e3ec3f04f32d7397908b724
Yousef Sami <yousef.sami19@gmail.com>
2018-01-02 22:23:43 +0330

    Change directory name (#23)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AmazingTimePickerModule } from './dist-library/atp-time-picker.module';
+import { AmazingTimePickerModule } from './atp-library/atp-time-picker.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
