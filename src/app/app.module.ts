1dca3268dd8793aa0ac4effbc39b04f833f00e70
Yousef Sami <yousef.sami19@gmail.com>
2018-01-14 23:58:18 +0330

    A (#25)
    * Change template
    
    * passed linting
    
    * publishing to npm testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AmazingTimePickerModule } from './atp-library/atp-time-picker.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './presentation/tabs/tabs.component';
+import { TabComponent } from './presentation/tab/tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    TabComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/presentation/tabs/tabs.component.ts
@@ -0,0 +1,31 @@
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss']
+})
+export class TabsComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
+
+  // contentChildren are set
+  ngAfterContentInit() {
+    // get all active tabs
+    const activeTabs = this.tabs.filter((tab) => tab.active);
+
+    // if there is no active tab set, activate the first
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    // deactivate all tabs
+    this.tabs.toArray().forEach( tabs => tabs.active = false);
+
+    // activate the tab the user has clicked on.
+    tab.active = true;
+  }
+
+}
