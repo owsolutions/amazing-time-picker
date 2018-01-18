664e55e0e77a6cada91a600d0c6ef8b8292f29af
anonymouse <anonymouse>
2018-01-19 03:18:07 +0330

    Update the github pages

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ set -e # exit with nonzero exit code if anything fails
 
 if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
   npm run build:github;
+  git checkout -b gh-pages;
+  git add -A;
+  git commit -m "Build github pages";
+  
   git push https://torabian:${GITHUB_TOKEN}@github.com/owsolutions/amazing-time-picker :gh-pages && git subtree push --prefix dist https://torabian:${GITHUB_TOKEN}@github.com/owsolutions/amazing-time-picker gh-pages
   
 else
