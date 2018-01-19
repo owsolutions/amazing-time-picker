b650fe18a10c17db3e8056160297f0e20ef9dcdf
anonymouse <anonymouse>
2018-01-19 07:26:20 +0330

    If anything fails we stop

--- scripts/angular4-test.sh
+++ scripts/angular4-test.sh
@@ -4,7 +4,7 @@
 # We assume that angular-cli is globally installed.
 # We are not cloning existing projects, we build with latest version of angular-cli
 # and install this dependency
-sudo -e;
+set -e;
 
 npm run packagr;
 cd /tmp/;
