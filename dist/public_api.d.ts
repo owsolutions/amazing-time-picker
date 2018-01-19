661eddb005a312682df2abe1fc35a830e7507102
yousef <yousef.sami19@gmail.com>
2018-01-19 12:19:36 +0330

    Change Packagr destination when transpiled package

--- dist/public_api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './src/app/atp-library/atp-time-picker.module';
-export * from './src/app/atp-library/atp-time-picker.service';
--- dist/src/app/atp-library/atp-time-picker.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Injector, ApplicationRef, ComponentFactoryResolver } from '@angular/core';
-import { TimePickerConfig, IDialogResult } from './definitions';
-export declare class AmazingTimePickerService {
-    private resolver;
-    private appRef;
-    private injector;
-    constructor(resolver: ComponentFactoryResolver, appRef: ApplicationRef, injector: Injector);
-    open(config?: TimePickerConfig): IDialogResult;
-}
