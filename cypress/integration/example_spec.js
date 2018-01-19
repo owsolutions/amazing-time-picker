fcc2578b95e6180bf8ffed397b4ea4d5cceecc51
anonymouse <anonymouse>
2018-01-19 05:50:17 +0330

    Update the test cases for angular 4 and angular 5

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -29,11 +29,8 @@ describe('Kitchen Sink', function () {
   it('Open dialog and selecting a time must update input via directive', function () {
 
     cy.visit('http://localhost:8080');
-
     cy.title().should('include', 'Amazing TimePicker')
-    
     cy.get('button.test-open-dialog').click();
-
     for (const number of clockMaker('hour')) {
       cy.get('.time-picker-clock').click(number.top, number.left);
       cy.wait(50);
@@ -41,6 +38,32 @@ describe('Kitchen Sink', function () {
     cy.get('.atp-ref-dialog-close').click();
     cy.wait(100);
     cy.get('#atp-ref-dialog-result').should('have.value', '09:00');
-    
-  })
+  
+  });
+
+  it ('Project on Angular 4 must work proprely', function () {
+    cy.visit('http://localhost:12001');
+    cy.title().should('include', 'Amazing TimePicker')
+    cy.get('button.test-open-dialog').click();
+    for (const number of clockMaker('hour')) {
+      cy.get('.time-picker-clock').click(number.top, number.left);
+      cy.wait(50);
+    }
+    cy.get('.atp-ref-dialog-close').click();
+    cy.wait(100);
+    cy.get('#atp-ref-dialog-result').should('have.value', '09:00');
+  });
+
+  it ('Project on Angular 5 must work proprely', function () {
+    cy.visit('http://localhost:12002');
+    cy.title().should('include', 'Amazing TimePicker')
+    cy.get('button.test-open-dialog').click();
+    for (const number of clockMaker('hour')) {
+      cy.get('.time-picker-clock').click(number.top, number.left);
+      cy.wait(50);
+    }
+    cy.get('.atp-ref-dialog-close').click();
+    cy.wait(100);
+    cy.get('#atp-ref-dialog-result').should('have.value', '09:00');
+  });
 });
