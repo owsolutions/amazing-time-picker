47c876934d96965bec46193d51ea379af0980a6c
anonymouse <anonymouse>
2018-01-19 05:34:43 +0330

    Initial testing for testbeds

--- scripts/angular4-test.sh
+++ scripts/angular4-test.sh
@@ -6,8 +6,35 @@
 # and install this dependency
 
 
-# Create a workspace for project
+# Get the latest version of amazing-time-picker
+# For a pull request, you can copy the amazing-time-picker/dist to /tmp/
+
+# -------------------------- Use for independent of Travis
+# cd /tmp/;
+# rm -rf amazing-time-picker;
+# git clone https://github.com/owsolutions/amazing-time-picker --depth=1;
+# cd amazing-time-picker;
+# npm install;
+# npm run build;
+# npm run packagr;
+# ---------------------------- End
+
+# Now you have the package ready at:
+# /tmp/amazing-time-picker/dist
+# Prepare the test beds;
+npm run packagr;
 cd /tmp/;
-rm -rf ng4;
-# Install
-ng new ng4
+git clone https://github.com/owsolutions/amazing-time-picker-test-beds --depth=1
+cd amazing-time-picker-test-beds;
+
+cd ng4-test-bed;
+npm install;
+npm install /tmp/amazing-time-picker/dist;
+npm start &;
+
+cd ../ng5-test-bed;
+npm install;
+npm install /tmp/amazing-time-picker/dist;
+npm start &;
+
+echo "Now we have lifted all test beds!";
