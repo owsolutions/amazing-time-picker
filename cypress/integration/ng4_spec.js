8d9c5ef6d8f93c21d2cc841075cc6af2afdf3373
anonymouse <anonymouse>
2018-01-24 22:33:31 +0330

    Add the test beds

---
+++ cypress/integration/ng4_spec.js
@@ -0,0 +1,17 @@
+const { clockMaker } = require('./shared');
+
+describe('Kitchen Sink', function () {
+
+  it ('Project on Angular 4 must work proprely', function () {
+    cy.visit('http://localhost:12001');
+    cy.title().should('include', 'Ng5TestBed')
+    cy.get('#test-bed-directive').click();
+    for (const number of clockMaker('hour')) {
+      cy.get('.time-picker-clock').click(number.top, number.left);
+      cy.wait(50);
+    }
+    cy.get('.atp-ref-dialog-close').click();
+    cy.wait(100);
+    cy.get('#test-bed-directive').should('have.value', '09:16');
+  });
+});
