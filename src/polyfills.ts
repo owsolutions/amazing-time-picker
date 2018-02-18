fe2e98f855d604e1215d92c98b0f48caa8f7fed0
anonymouse <anonymouse>
2018-02-18 16:43:28 +0330

    Package

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,6 +17,8 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+declare var require: any;
+require('es6-object-assign').polyfill();
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
 // import 'core-js/es6/symbol';
