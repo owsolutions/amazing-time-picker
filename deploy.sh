2cccdeab3fb39c5d700e8ce21ce7c80b6704a737
gmfun <gaurav@hypertrack.io>
2018-05-08 18:06:53 +0530

    fix script in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
   git checkout -b gh-pages;
   git add -A;
   git commit -m "Build github pages";
-  
-  git push https://torabian:${GITHUB_TOKEN}@github.com/owsolutions/amazing-time-picker :gh-pages && git subtree push --prefix dist https://torabian:${GITHUB_TOKEN}@github.com/owsolutions/amazing-time-picker gh-pages
-  
+
+  git push https://torabian:${GITHUB_TOKEN}@github.com/owsolutions/amazing-time-picker :gh-pages && git subtree push --prefix dist/demo https://torabian:${GITHUB_TOKEN}@github.com/owsolutions/amazing-time-picker gh-pages
+
 else
  echo "Skipped updating gh-pages, because build is not triggered from the master branch."
 fi;
