c7eb5b3d6b189e41c88b515dbbdfe7af2cb3f5f9
Ali <ali.torabi>
2018-12-21 17:32:39 +0100

    Update

---
+++ src/app/presentation/full-layout/full-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-layout',
+  templateUrl: './full-layout.component.html',
+  styleUrls: ['./full-layout.component.scss']
+})
+export class FullLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/presentation/layout-basic/layout-basic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-basic',
+  templateUrl: './layout-basic.component.html',
+  styleUrls: ['./layout-basic.component.scss']
+})
+export class LayoutBasicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
