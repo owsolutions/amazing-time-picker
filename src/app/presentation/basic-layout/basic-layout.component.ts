09afe603c7799d9c9d0c1ab5caf249394fefe661
Ali <ali.torabi>
2018-12-22 10:42:41 +0100

    Build

--- src/app/presentation/basic-layout/basic-layout.component.ts
+++ src/app/presentation/basic-layout/basic-layout.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic-layout.component.scss']
 })
 export class LayoutBasicComponent implements OnInit {
+  public Directive = `<input atp-time-picker value="19:00"/>`;
+  public appModule = `import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AmazingTimePickerModule } from 'amazing-time-picker'; // this line you need
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AmazingTimePickerModule // this line you need
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }`;
   constructor() {}
 
   ngOnInit() {}
