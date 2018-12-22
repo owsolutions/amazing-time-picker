885ca9691f279e315fcdf39744a60c6acbce3e91
Ali <ali.torabi>
2018-12-22 11:48:48 +0100

    Update

--- src/app/atp-library/atp.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AtpDirective } from './atp.directive';
-
-describe('AtpDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AtpDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  hmr: false,
-  layout: 'full'
-};
